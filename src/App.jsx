fb336bd156a72599399d5a4528ef9bfee44adeac
Artli22 <ludinlima2514@gmail.com>
2026-06-05 18:32:42 -0600

    Esqueleto general del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from './Componentes/navBar'
+import Hero from './Componentes/hero'
+import SobreMi from './Componentes/sobreMi'
+import Experiencia from './Componentes/experiencia'
+import Proyectos from './Componentes/proyectos'
+import Footer from './Componentes/footer'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <SobreMi />
+      <Experiencia />
+      <Proyectos />
+      <Footer />
+    </>
+  )
+}
+
+export default App
