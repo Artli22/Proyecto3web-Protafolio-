e6daa234640efd4c178e425921718510a0b55cfb
Artli22 <ludinlima2514@gmail.com>
2026-06-06 12:54:24 -0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './Componentes/hero'
 import SobreMi from './Componentes/sobreMi'
 import Pasatiempos from './Componentes/pasatiempos'
 import Proyectos from './Componentes/proyectos'
+import Footer from './Componentes/footer'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <SobreMi />
       <Pasatiempos />
       <Proyectos />
+      <Footer />
     </>
   )
 }
---
+++ src/Componentes/footer.jsx
@@ -0,0 +1,47 @@
+import '../Estilos/footer.css'
+import { Mail } from 'lucide-react'
+import { FiGithub, FiInstagram } from 'react-icons/fi'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-content">
+          <h2 className="footer-title">¡Contáctame!</h2>
+          
+          <p className="footer-text">
+            Si te gustaron las demos y tienes una buena idea puedes contactarme mediante los siguientes medios
+          </p>
+
+          <div className="footer-contact">
+            <div className="contact-item">
+              <Mail size={20} />
+              <span>Teléfono: 5555-3242</span>
+            </div>
+          </div>
+
+          <div className="footer-socials">
+            <a 
+              href="https://instagram.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+              className="social-link"
+            >
+              <FiInstagram size={28} />
+            </a>
+            <a 
+              href="https://github.com/Artli22" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+              className="social-link"
+            >
+              <FiGithub size={28} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
